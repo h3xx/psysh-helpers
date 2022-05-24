f2199923e1c5b0bbb90b3ed4d753197cfa8d00c1
Dan Church <dan.church@irishtitan.com>
2022-05-24 10:24:45 -0500

    magento: Allow setting area code

--- magento.d/areacode.php
+++ magento.d/areacode.php
@@ -1,5 +1,17 @@
 <?php
 
-function areacode(string $areaCode): void {
+function areacode(string $areaCode = null) {
+    if (is_null($areaCode)) {
+        return getAreacode();
+    } else {
+        setAreaCode($areaCode);
+    }
+}
+
+function setAreacode(string $areaCode): void {
     state()->setAreaCode($areaCode);
+}
+
+function getAreacode() {
+    state()->getAreaCode();
 }
--- magento.d/zzfix-areacode.php
+++ magento.d/zzfix-areacode.php
@@ -1,4 +1,4 @@
 <?php
 
-areacode('frontend');
+setAreaCode('frontend');
 
