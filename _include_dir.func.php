2956e6fd83d4a67621c87da51cd9938fd58f044d
Dan Church <dan.church@irishtitan.com>
2022-07-06 14:00:04 -0500

    Add support for gathering config array from ./*.d/*.php

--- _include_dir.func.php
+++ _include_dir.func.php
@@ -1,9 +1,12 @@
 <?php
 
-function _include_dir(string $dir): void {
+function _include_dir(string $dir, ?Closure $callback = null) {
     if (is_dir($dir)) {
         foreach (glob("$dir/*.php") as $inc) {
-            require_once($inc);
+            $result = require_once($inc);
+            if (!is_null($callback)) {
+                $callback($result, $inc, $dir);
+            }
         }
     }
 }
